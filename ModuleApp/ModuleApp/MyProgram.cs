a272e6ef1cf1c0f1d60b69c4e36bf7964d180cb0
bang-dev <anhbangluckystar@gmail.com>
2023-07-08 00:09:19 +0700

    Update

---
+++ ModuleApp/ModuleApp/MyProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ModuleApp
+{
+    internal class MyProgram
+    {
+        public static void Main(string[] args)    
+        {
+            Console.WriteLine("Welcome");
+
+            Console.ReadKey();
+        }
+    }
+}
